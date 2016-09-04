47bd4e6ae3e22f3d2c5aeb32c5b267dfcb555f0c
Dawid Dyrcz <dyrcu15@gmail.com>
2016-09-04 18:05:32 +0200

    Communication between client and server

--- client/app.js
+++ client/app.js
@@ -1,23 +1,29 @@
-var os = require('os');
-var osUtils = require('os-utils');
+var os = require('os'),
+    osUtils = require('os-utils'),
+    ioc = require('socket.io-client');
 
-console.log(os.hostname());
-console.log(`PLATFORM: ${os.platform()}`);
+var socket = ioc.connect("http://localhost:8000");
 
-setInterval(() => {
+socket.on("connect", function () {
 
-    var totalMemory = Math.round(osUtils.totalmem());
-    var freeMemory = Math.round(osUtils.freemem());
+    console.log('Connected to server');
 
-    console.log(`TOTAL MEMORY: ${totalMemory}MB`);
-    console.log(`MEMORY IN USE: ${totalMemory - freeMemory}MB`);
+    socket.emit('hostname', os.hostname());
+    socket.emit('platform', os.platform());
 
-    // sent information to the server
-}, 1000);
+    setInterval(() => {
+        // var totalMemory = Math.round(osUtils.totalmem());
+        // var freeMemory = Math.round(osUtils.freemem());
 
-setInterval(() =>
-    osUtils.cpuUsage(function (v) {
-        console.log('CPU Usage (%): ' + v);
+        //console.log(`TOTAL MEMORY: ${totalMemory}MB`);
+        //console.log(`MEMORY IN USE: ${totalMemory - freeMemory}MB`);
 
-        // sent information to the server
-    }), 1000);
+        // sent information to the server        
+
+    }, 1000);
+
+    setInterval(() =>
+        osUtils.cpuUsage(function (usage) {
+            socket.emit('cpu', Math.round(usage * 100));
+        }), 1000);
+});
--- server/server.js
+++ server/server.js
@@ -1,51 +1,29 @@
-// var socketio = require('socket.io');
-// var http = require('http');
+var socketio = require('socket.io');
+var http = require('http');
 
-// var server = http.createServer((req, res) => {
-	
-// });
+var server = http.createServer();
 
+server.listen(8000, () => {
+    console.log('Server is listining');
+});
 
-// var io = socketio.listen(server);
+var io = socketio.listen(server);
 
-// var clients = [];
+var clients = [];
 
-// io.sockets.on('connection', (socket) => {
-//     clients.push(socket);
+io.sockets.on('connection', (socket) => {
 
+    console.log('Connection accepted');
 
-//     socket.on('cpu', (cpu) => {
-//         //send to dashboard page
-//     });
+    socket.on('hostname', (hostname) => {
+        console.log(hostname);
+    });
 
-// });
+    socket.on('platform', (platform) => {
+        console.log(platform);
+    });
 
-var port = 3011;
-
-var server = require( 'http' ).createServer( ).listen( port, function () {
-    console.log( "Express server listening on port " + port );
-} );
-
-var io = require( 'socket.io' ).listen( server );
-
-io.sockets.on( "connection", function ( socket ) {
-    console.log( 'Server: Incoming connection.' );
-    socket.on( "echo", function ( msg, callback ) {
-        callback( msg );
-    } );
-} );
-
-
-var ioc = require( 'socket.io-client' );
-var client = ioc.connect( "http://localhost:" + port );
-
-client.once( "connect", function () {
-    console.log( 'Client: Connected to port ' + port );
-
-    // client.emit( "echo", "Hello World", function ( message ) {
-    //     console.log( 'Echo received: ', message );
-    //     client.disconnect();
-    //     server.close();
-    // } );
-} );
-
+    socket.on('cpu', (usage) => {
+        console.log(usage);
+    });
+});
