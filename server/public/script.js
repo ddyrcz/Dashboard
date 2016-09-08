8d4bc922f008727c2cecd798831114a5fee9ab6f
Dawid Dyrcz <dyrcu15@gmail.com>
2016-09-08 17:29:50 +0200

    Display cpu usage as a  progress bar

--- server/public/script.js
+++ server/public/script.js
@@ -1,50 +1,33 @@
-//var socket = io.connect();
+var socket = io.connect();
+
+var bar = new ProgressBar.Line(container, {
+  strokeWidth: 4,
+  easing: 'easeInOut',
+  duration: 1400,
+  progress: 1,
+  color: '#FFEA82',
+  trailColor: '#eee',
+  trailWidth: 1,
+  svgStyle: {width: '100%', height: '100%'},
+  from: {color: '#FFEA82'},
+  to: {color: '#ED6A5A'},
+  step: (state, bar) => {
+    bar.path.setAttribute('stroke', state.color);
+  }
+});
 
 $(document).ready(() => {
-    // socket.emit('dashboard');
+    socket.emit('dashboard');
 
     // socket.on('hostname', (data) => {        
     //      $('#clients').append(`<li id='${data.id}'><div class='hostname'>${data.hostname}</div><div class='cpu'></div></li>`);
     // });
 
-    // socket.on('cpu', (data) => {
-    //     console.log(`#${data.id} .cpu`);
-    //     $(`#${data.id} .cpu`).text(data.usage);        
-    // });
+    socket.on('cpu', (data) => {
+        // console.log(`#${data.id} .cpu`);
+        // $(`#${data.id} .cpu`).text(data.usage);    
 
-// progressbar.js@1.0.0 version is used
-// Docs: http://progressbarjs.readthedocs.org/en/1.0.0/
-
-var bar = new ProgressBar.Circle(container, {
-  color: '#aaa',
-  // This has to be the same size as the maximum width to
-  // prevent clipping
-  strokeWidth: 4,
-  trailWidth: 1,
-  easing: 'easeInOut',
-  duration: 1400,
-  text: {
-    autoStyleContainer: false
-  },
-  from: { color: '#aaa', width: 1 },
-  to: { color: '#333', width: 4 },
-  // Set default step function for all animate calls
-  step: function(state, circle) {
-    circle.path.setAttribute('stroke', state.color);
-    circle.path.setAttribute('stroke-width', state.width);
-
-    var value = Math.round(circle.value() * 100);
-    if (value === 0) {
-      circle.setText('');
-    } else {
-      circle.setText(value);
-    }
-
-  }
-});
-bar.text.style.fontFamily = '"Raleway", Helvetica, sans-serif';
-bar.text.style.fontSize = '2rem';
-
-bar.animate(1.0);  // Number from 0.0 to 1.0
+        bar.animate((data.usage/100), {duration:800});
+    });
 
 });
